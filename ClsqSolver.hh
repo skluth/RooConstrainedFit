914c6cfca42ea8d5e36273a2c67f54542443a416
Alessandro Silenzi <alessandro.silenzi@desy.de>
2012-10-11 15:04:41 +0200

    changed title of PrintTitle to printTitle and starting to translate the member functions from the clsq.py

--- ClsqSolver.hh
+++ ClsqSolver.hh
@@ -39,7 +39,6 @@ private:
 class ClsqSolver {
 
 public:
-
   // Ctor: pass input data, constraint function object and optional
   // parameter name maps mapping parameter number (0-n) to names:
   ClsqSolver( const TVectorD& d, 
@@ -54,7 +53,9 @@ public:
   // Accessors:
   std::vector<std::string> getMParNames() const;
   std::vector<std::string> getUParNames() const;
-  void PrintTitle() const;
+  void printTitle() const;
+  void printFitParameters(Double_t Chisq , Int_t NDOF , const char* format);
+  void printResults(const char * format , Bool_t cov=false, Bool_t corr=false );
   
 
 
@@ -64,10 +65,46 @@ public:
   TVectorD                 getUParv() const;
   double*                  getUPar();
   double*                  getMPar();
+  double*                  getUparErrors() const;
   double*                  getMparErrors() const;
   TMatrixDSym              getUparErrorMatrix() const;
   TMatrixDSym              getMparErrorMatrix() const;
   TMatrixDSym              getCovMatrix() const;
+  void solveByPartition( TVectorD& dcdmpm, TVectorD& dcdupm, TVectorD& constrv );
+  void prepareDeltapar( Int_t& datadim, Int_t& upardim, Int_t& constrdim,
+			Double_t& c11, Double_t& c21, Double_t& c31, 
+			Double_t& c32, Double_t& c33, TVectorD& constrv );
+  void solve( Bool_t& lpr, Bool_t& lBlobel);
+  void fixMpar(Int_t parspec, Double_t *val=NULL, Bool_t lpr=true );
+  void fixMpar(std::string parspec,Double_t *val=NULL, Bool_t lpr=true );
+  void releaseMpar(Int_t parspec, Bool_t lpr=true );
+  void releaseMpar(std::string parspec, Bool_t lpr=true );
+  void fixUpar(Int_t parspec, Double_t *val=NULL, Bool_t lpr=true );
+  void fixUpar(std::string parspec, Double_t *val=NULL, Bool_t lpr=true );
+  void releaseUpar(Int_t parspec, Bool_t lpr=true );
+  void releaseUpar(std::string parspec, Bool_t lpr=true );
+    //def prepareRhsv( self, dim, datadim, upardim, constrv ):
+    //def calcChisq( self, dcdmpm, c33 ):
+    //def getConstraints( self ):
+    //def getErrorMatrix( self ):
+    //def getUparCorrMatrix( self ):
+    //def getMparCorrMatrix( self ):
+    //def getCorrMatrix( self ):
+    //def getCovm( self ):
+    //def getInvm( self ):
+    //def getData( self ):
+    //def getMparPulls( self ):
+    //def makeInverseProblemMatrix( self, dcdmpm, dcdupm ):
+    //def setUpar( self, parspec, val ):
+    //def setMpar( self, parspec, val ):
+    //def profileUpar( self, parspec, nstep=5, stepsize=1.0 ):
+    //def profileMpar( self, parspec, nstep=5, stepsize=1.0 ):
+    //def minosUpar( self, parspec, chisqmin=None, delta=1.0 ):
+    //def minosMpar( self, parspec, chisqmin=None, delta=1.0 ):
+    //def fun( x ):
+    //def contour( self, iparspec, iopt, jparspec, jopt,
+    //def profile2d( self, ipar, iopt, jpar, jopt, nstep=11, stepsize=0.5 ):
+
 
 private:
   
@@ -96,13 +133,17 @@ private:
 
   // Instance Methods
   Double_t calcChisq( TVectorD& dcdmpm, Double_t c33 );
-  void solve( Bool_t& lpr, Bool_t& lBlobel);
   void solveByInversion( TVectorD& dcdmpm,  TVectorD& dcdupm, TVectorD& constrv );
-  void solveByPartition( TVectorD& dcdmpm, TVectorD& dcdupm, TVectorD& constrv );
+  Int_t parameterIndex(std::string parspec);
+  Int_t parameterIndex(Int_t parspec);
+    //def __setParNames( self, parnames, npar ):
+    //def __printPars( self, par, parerrs, parnames, fixedParFunctions,
+    //def __printMatrix( self, m, ffmt, names ):
+    //def __getCorrMatrix( self, paropt ):
+    //def __makeProblemMatrix( self, dcdmpm, dcdupm ):
+    //def __profile( self, ipar, nstep, stepsize,
+    //def __minos( self, ipar, chisqmin, delta,
 
-  void prepareDeltapar( Int_t& datadim, Int_t& upardim, Int_t& constrdim,
-			Double_t& c11, Double_t& c21, Double_t& c31, 
-			Double_t& c32, Double_t& c33, TVectorD& constrv );
 };
 
 
